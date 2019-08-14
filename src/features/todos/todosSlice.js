f53db013e42976f4e5830ba2e6d92f2f7695a10e
Mark Erikson <mark@isquaredsoftware.com>
2019-08-13 23:10:32 -0400

    Add an initial todos slice

---
+++ src/features/todos/todosSlice.js
@@ -0,0 +1,22 @@
+import { createSlice } from 'redux-starter-kit'
+
+const todosSlice = createSlice({
+  slice: 'todos',
+  initialState: [],
+  reducers: {
+    addTodo(state, action) {
+      const { id, text } = action.payload
+      state.push({ id, text, completed: false })
+    },
+    toggleTodo(state, action) {
+      const todo = state.find(todo => todo.id === action.payload.id)
+      if (todo) {
+        todo.completed = !todo.completed
+      }
+    }
+  }
+})
+
+export const { addTodo, toggleTodo } = todosSlice.actions
+
+export default todosSlice.reducer
