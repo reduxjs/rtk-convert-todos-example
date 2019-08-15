accc5896b5d092e736ec2d24d10246ff18aec7cf
Mark Erikson <mark@isquaredsoftware.com>
2019-08-14 23:09:22 -0400

    Consolidate components into feature folders

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,7 @@
 import React from 'react'
-import Footer from './Footer'
-import AddTodo from '../containers/AddTodo'
-import VisibleTodoList from '../containers/VisibleTodoList'
+import Footer from 'features/filters/Footer'
+import AddTodo from 'features/todos/AddTodo'
+import VisibleTodoList from 'features/todos/VisibleTodoList'
 
 const App = () => (
   <div>
