46bcaf7f36dbe250cd16c2e04f493da5054594e9
Mark Erikson <mark@isquaredsoftware.com>
2019-08-14 22:47:32 -0400

    Convert visible todos to a memoized selector
    # Conflicts:
    #	src/containers/VisibleTodoList.js

--- src/containers/VisibleTodoList.js
+++ src/containers/VisibleTodoList.js
@@ -1,28 +1,33 @@
 import { connect } from 'react-redux'
-import { toggleTodo } from '../actions'
+import { createSelector } from 'redux-starter-kit'
+import { toggleTodo } from 'features/todos/todosSlice'
 import TodoList from '../components/TodoList'
 import { VisibilityFilters } from 'features/filters/filtersSlice'
 
-const getVisibleTodos = (todos, filter) => {
-  switch (filter) {
-    case VisibilityFilters.SHOW_ALL:
-      return todos
-    case VisibilityFilters.SHOW_COMPLETED:
-      return todos.filter(t => t.completed)
-    case VisibilityFilters.SHOW_ACTIVE:
-      return todos.filter(t => !t.completed)
-    default:
-      throw new Error('Unknown filter: ' + filter)
+const selectTodos = state => state.todos
+const selectFilter = state => state.visibilityFilter
+
+const selectVisibleTodos = createSelector(
+  [selectTodos, selectFilter],
+  (todos, filter) => {
+    switch (filter) {
+      case VisibilityFilters.SHOW_ALL:
+        return todos
+      case VisibilityFilters.SHOW_COMPLETED:
+        return todos.filter(t => t.completed)
+      case VisibilityFilters.SHOW_ACTIVE:
+        return todos.filter(t => !t.completed)
+      default:
+        throw new Error('Unknown filter: ' + filter)
+    }
   }
-}
+)
 
 const mapStateToProps = state => ({
-  todos: getVisibleTodos(state.todos, state.visibilityFilter)
+  todos: selectVisibleTodos(state)
 })
 
-const mapDispatchToProps = dispatch => ({
-  toggleTodo: id => dispatch(toggleTodo(id))
-})
+const mapDispatchToProps = { toggleTodo }
 
 export default connect(
   mapStateToProps,
