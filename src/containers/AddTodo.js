1ebf475b37063888265b6ed99ff4568acde8a5e9
Mark Erikson <mark@isquaredsoftware.com>
2019-08-14 21:47:54 -0400

    Update AddTodo to dispatch the new action type
    - Replaced use of `dispatch` with `mapDispatch`
    - Converted the input to be controlled instead of uncontrolled

--- src/containers/AddTodo.js
+++ src/containers/AddTodo.js
@@ -1,27 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { connect } from 'react-redux'
-import { addTodo } from '../actions'
+import { addTodo } from 'features/todos/todosSlice'
 
-const AddTodo = ({ dispatch }) => {
-  let input
+const mapDispatch = { addTodo }
+
+const AddTodo = ({ addTodo }) => {
+  const [todoText, setTodoText] = useState('')
+
+  const onChange = e => setTodoText(e.target.value)
 
   return (
     <div>
       <form
         onSubmit={e => {
           e.preventDefault()
-          if (!input.value.trim()) {
+          if (!todoText.trim()) {
             return
           }
-          dispatch(addTodo(input.value))
-          input.value = ''
+          addTodo(todoText)
+          setTodoText('')
         }}
       >
-        <input ref={node => (input = node)} />
+        <input value={todoText} onChange={onChange} />
         <button type="submit">Add Todo</button>
       </form>
     </div>
   )
 }
 
-export default connect()(AddTodo)
+export default connect(
+  null,
+  mapDispatch
+)(AddTodo)
