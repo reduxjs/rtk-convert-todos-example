2af28fe0f71ebf03ae0e66874a73aca6925fba9a
Mark Erikson <mark@isquaredsoftware.com>
2019-08-14 21:25:53 -0400

    Implement addTodo ID generation

--- src/features/todos/todosSlice.js
+++ src/features/todos/todosSlice.js
@@ -1,12 +1,19 @@
 import { createSlice } from 'redux-starter-kit'
 
+let nextTodoId = 0
+
 const todosSlice = createSlice({
   slice: 'todos',
   initialState: [],
   reducers: {
-    addTodo(state, action) {
-      const { id, text } = action.payload
-      state.push({ id, text, completed: false })
+    addTodo: {
+      reducer(state, action) {
+        const { id, text } = action.payload
+        state.push({ id, text, completed: false })
+      },
+      prepare(text) {
+        return { payload: { text, id: nextTodoId++ } }
+      }
     },
     toggleTodo(state, action) {
       const todo = state.find(todo => todo.id === action.payload.id)
